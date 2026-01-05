98d8eaa59c74dac17c47abd5989851f86d68a98b
soulhn <88018456+soulhn@users.noreply.github.com>
2026-01-06 02:47:49 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 9.05 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -1,11 +1,3 @@
 def solution(dot):
-    x, y = dot
-
-    if x > 0 and y > 0:
-        return 1
-    elif x < 0 and y > 0:
-        return 2
-    elif x < 0 and y < 0:
-        return 3
-    else:
-        return 4
+    answer = 0
+    return answer
